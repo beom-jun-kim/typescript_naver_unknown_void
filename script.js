35c59d7fce1210deaed5d2f7a6985b9ef5cc4191
kimbeomjun <gimb52043@gmail.com>
2022-10-31 22:12:29 +0900

    Typescript_unknown_void_never

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // unknown : 변수의 타입을 미리 정하지 못할때
 
-// let a: unknown;
+let a: unknown;
 
-// if (typeof a === 'number') {
-//     let b = a + 1;
-// }
+if (typeof a === 'number') {
+    let b = a + 1;
+}
 
-// if (typeof a === 'string') {
-//     let b = a.toUpperCase();
-// }
+if (typeof a === 'string') {
+    let b = a.toUpperCase();
+}
 
 
 // void : 아무것도 return하지 않는 함수를 대상으로 사용
@@ -17,15 +17,15 @@
 // }
 
 // never : 함수가 절대 return 하지 않을 때 발생
-// function hello(name: string | number) {
-//     // return "x"
-//     // throw new Error("xxx");
-//     if (typeof name === "string") {
+function hello(name: string | number) {
+    // return "x"
+    // throw new Error("xxx");
+    if (typeof name === "string") {
 
-//     } else if (typeof name === "number") {
+    } else if (typeof name === "number") {
 
-//     } else {
-//         name
-//     }
-// }
+    } else {
+        name
+    }
+}
 
